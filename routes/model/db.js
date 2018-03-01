23a8df3282af5920ad15d9f6cec3f136228d2293
Tolunay Güdük <guduktolunay@gmail.com>
2018-03-02 01:49:40 +0300

    refactor to db js

--- routes/model/db.js
+++ routes/model/db.js
@@ -1,14 +1,9 @@
 var mysql = require('mysql');
 var global_variables = require('../../global_variables');
 
-var connection;
-connection = mysql.createConnection(global_variables.db_config);
+var connection = mysql.createConnection(global_variables.db_config);
 connection.connect(function (err) {
-if (err){
-console.log('Database connection error:',err);
-}
-else
-console.log('... SQL CONNECTED ...');
-
+    if (err){console.log('Database connection error:',err);}
+    else console.log('... SQL CONNECTED ...');
 });
 module.exports = connection;
